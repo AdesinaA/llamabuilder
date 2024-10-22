a3e89c74970b3be999be4370b7d50206e96ea4b8
Adesina Abdulrahman <53907121+AdesinaA@users.noreply.github.com>
2024-10-22 09:11:32 +0100

    Add files via upload

---
+++ components/Footer.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="mb-3 mt-5 flex h-16 w-full flex-col items-center justify-between space-y-3 px-3 pt-4 text-center sm:mb-0 sm:h-20 sm:flex-row sm:pt-2">
+      <div>
+        <div className="font-medium">
+          Built with{" "}
+          <a
+            href="https://dub.sh/together-ai/?utm_source=example-app&utm_medium=llamacoder&utm_campaign=llamacoder-app-signup"
+            className="font-semibold text-green-600 underline-offset-4 transition hover:text-gray-700 hover:underline"
+          >
+            Llama 3.1
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://dub.sh/together-ai/?utm_source=example-app&utm_medium=llamacoder&utm_campaign=llamacoder-app-signup"
+            className="font-semibold text-green-600 underline-offset-4 transition hover:text-gray-700 hover:underline"
+          >
+            Together AI
+          </a>
+          .
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+import GithubIcon from "./github-icon";
+
+export default function Header() {
+  return (
+    <header className="relative mx-auto mt-5 flex w-full items-center justify-center px-2 pb-7 sm:px-4">
+      <a
+        href="https://github.com/adesinaa/llmbuilder"
+        target="_blank"
+        className="ml-auto hidden items-center gap-3 rounded-2xl bg-white px-6 py-2 sm:flex"
+      >
+        <GithubIcon className="h-4 w-4" />
+        <span>GitHub Repo</span>
+      </a>
+    </header>
+  );
+}
