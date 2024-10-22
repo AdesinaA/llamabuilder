82adc4b6033034b4964d3e724bcd28ee1fb02356
Adesina Abdulrahman <53907121+AdesinaA@users.noreply.github.com>
2024-10-22 09:13:35 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="relative mx-auto mt-5 flex w-full items-center justify-center px-2 pb-7 sm:px-4">
       <a
-        href="https://github.com/adesinaa/llmbuilder"
+        href="https://github.com/adesinaa/llamabuilder"
         target="_blank"
         className="ml-auto hidden items-center gap-3 rounded-2xl bg-white px-6 py-2 sm:flex"
       >
